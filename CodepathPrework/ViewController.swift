28f3ac81b802219fc5e1fd0c86568d5362432a3b
Amani M <51408483+ambonimp@users.noreply.github.com>
2025-01-26 06:21:54 -0500

    Initial Commit

---
+++ CodepathPrework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodepathPrework
+//
+//  Created by Amani Mbonimpa on 1/26/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CodepathPreworkTests/CodepathPreworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodepathPreworkTests.swift
+//  CodepathPreworkTests
+//
+//  Created by Amani Mbonimpa on 1/26/25.
+//
+
+import Testing
+@testable import CodepathPrework
+
+struct CodepathPreworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
