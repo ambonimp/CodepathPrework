a048ed6e783c8028573dcb40634f68b991e4c39e
Amani M <51408483+ambonimp@users.noreply.github.com>
2025-01-26 07:24:46 -0500

    Add color changing button

--- CodepathPrework/ViewController.swift
+++ CodepathPrework/ViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    func changeColor() -> UIColor {
+        return UIColor(red : CGFloat.random(in : 0 ... 1), green : CGFloat.random(in : 0 ... 1), blue : CGFloat.random(in : 0 ... 1), alpha: 0.5)
+        
+    }
+    
+    @IBAction func changeBackgroundColor(_ sender: UIButton) {
+        view.backgroundColor = changeColor()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
